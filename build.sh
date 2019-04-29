0ae6578da6a4f77bbb51fb1ba34a1082b48c9392
Unknown <camberopiqueras@gmail.com>
2019-04-29 08:58:32 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 # Compile Elm files
 elm-make ./src/RockPaperScissors/RockPaperScissors.elm --output=./src/script.js --warn
+# Locate the repository
+LOC='file://'$PWD'/index.html'
+# Open in browser
+# Check if Google Chrome exists
+if ! type 'google-chrome' > /dev/null; then
+  	echo 'Google Chrome is not installed, the app can be opened with other browsers by visiting the following url: '$LOC
+else
+	google-chrome $LOC
+fi
